ba4fda645db66748e0827020c1e80fd6b742e7e5
rabok <flynnduerrel@gmail.com>
2023-05-25 14:56:06 +0300

    last comit befor i implement the blog

---
+++ app.js
@@ -0,0 +1,67 @@
+// Example data for timeline items
+const timelineData = [
+    {
+      date: "May 1, 2023",
+      time: "10:00 AM",
+      title: "Project A",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "project-a.jpg"
+    },
+    {
+      date: "May 5, 2023",
+      time: "2:30 PM",
+      title: "Project B",
+      description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+      image: "project-b.jpg"
+    },
+    // Add more timeline items as needed
+  ];
+  
+  // Function to create a timeline item
+  function createTimelineItem(item) {
+    const timelineContainer = document.getElementById("timeline-container");
+  
+    const timelineItem = document.createElement("div");
+    timelineItem.classList.add("timeline-item");
+  
+    const timelineImage = document.createElement("img");
+    timelineImage.src = item.image;
+    timelineImage.alt = item.title;
+    timelineItem.appendChild(timelineImage);
+  
+    const timelineContent = document.createElement("div");
+    timelineContent.classList.add("timeline-content");
+  
+    const timelineDate = document.createElement("p");
+    timelineDate.classList.add("timeline-date");
+    timelineDate.textContent = `${item.date} at ${item.time}`;
+    timelineContent.appendChild(timelineDate);
+  
+    const timelineTitle = document.createElement("h3");
+    timelineTitle.textContent = item.title;
+    timelineContent.appendChild(timelineTitle);
+  
+    const timelineDescription = document.createElement("p");
+    timelineDescription.textContent = item.description;
+    timelineContent.appendChild(timelineDescription);
+  
+    timelineItem.appendChild(timelineContent);
+  
+    timelineContainer.appendChild(timelineItem);
+  }
+  
+  // Function to update the timeline section with data
+  function updateTimeline() {
+    // Clear existing timeline items
+    const timelineContainer = document.getElementById("timeline-container");
+    timelineContainer.innerHTML = "";
+  
+    // Create new timeline items based on the data
+    timelineData.forEach(item => {
+      createTimelineItem(item);
+    });
+  }
+  
+  // Call the updateTimeline function to initially populate the timeline section
+  updateTimeline();
+  
